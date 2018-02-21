f75460cf3b2585b530e19c3392ce5c9c1000e711
X1008332 <giovanni.fedelethinkopen.it>
2018-02-21 15:03:14 +0100

    test rimozione node_modules

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("......" + session.message.text + ".....");
     session.send("......" + session.message.text + ".....");
-});
+}); 
